f8fb2e0455ac477bc58a84a33890ea35354a2762
Jackson Dolman <mail@dolmios.com>
2022-03-10 19:20:08 +1100

    chore: bump design system

--- src/pages/c/badge.tsx
+++ src/pages/c/badge.tsx
@@ -1,74 +1,61 @@
-import { Badge, Card, Element, Heading } from "@cosmo-au/design-system";
-import type { NextPage } from "next";
-import { CodeBlock } from "react-code-blocks";
+import { Badge, Card, Element, Heading } from '@cosmo-au/design-system';
+import type { NextPage } from 'next';
+import { CodeBlock } from 'react-code-blocks';
 
-import Layout from "../../components/Layout";
+import Layout from '../../components/Layout';
 
 function PagesComponentsBadge() {
   return (
     <Layout
       contents={[
         {
-          name: "Getting Started",
-          value: "#start",
+          name: 'Getting Started',
+          value: '#start',
         },
         {
-          name: "Basic Usage",
-          value: "#basic",
+          name: 'Basic Usage',
+          value: '#basic',
         },
         {
-          name: "Dot",
-          value: "#dot",
+          name: 'Dot',
+          value: '#dot',
         },
         {
-          name: "API Reference",
-          value: "#api",
+          name: 'API Reference',
+          value: '#api',
         },
       ]}
-      github="https://github.com/cosmo-au/design-system/blob/main/src/components/Badge/Badge.tsx"
-      name="Badge"
-      parent="Components"
-    >
-      <Element bottom={10} id="start">
+      github='https://github.com/cosmo-au/design-system/blob/main/src/components/Badge/Badge.tsx'
+      name='Badge'
+      parent='Components'>
+      <Element bottom={10} id='start'>
         <Heading level={2}>Getting Started</Heading>
         <Heading bottom={4} level={5}>
           Import the component
         </Heading>
-        <CodeBlock
-          language="javascript"
-          showLineNumbers
-          text="import { Badge } from '@cosmo-au/design-system';"
-        />
+        <CodeBlock language='javascript' showLineNumbers text="import { Badge } from '@cosmo-au/design-system';" />
       </Element>
-      <Element bottom={10} id="basic">
+      <Element bottom={10} id='basic'>
         <Heading level={2}>Basic Usage</Heading>
         <Element bottom={4}>
           <Card border>
-            <Badge theme="border">Basic Badge</Badge>
+            <Badge theme='border'>Basic Badge</Badge>
           </Card>
         </Element>
-        <CodeBlock
-          language="javascript"
-          showLineNumbers
-          text="<Badge theme='border'>Basic Badge</Badge>"
-        />
+        <CodeBlock language='javascript' showLineNumbers text='<Badge >Basic Badge</Badge>' />
       </Element>
-      <Element bottom={10} id="dot">
+      <Element bottom={10} id='dot'>
         <Heading level={2}>Dot</Heading>
         <Element bottom={4}>
           <Card border>
-            <Badge dot="pulse" theme="border">
+            <Badge dot='pulse' theme='border'>
               Badge with Dot
             </Badge>
           </Card>
         </Element>
-        <CodeBlock
-          language="javascript"
-          showLineNumbers
-          text="<Badge dot='pulse' theme='border'>Badge with Dot</Badge>"
-        />
+        <CodeBlock language='javascript' showLineNumbers text="<Badge dot='pulse' >Badge with Dot</Badge>" />
       </Element>
-      <Element bottom={10} id="api">
+      <Element bottom={10} id='api'>
         <Heading level={2}>API Reference</Heading>
         <Element bottom={4}>
           <Card border>Todo</Card>
